a248e41c92888aa383fac120e08575851008c56a
Greg Schuman <gcschuman@hotmail.com>
2023-03-09 12:52:18 -0800

    Added blank array for notes.

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-const keys = ['c-key', 'd-key', 'e-key', 'f-key', 'g-key', 'a-key', 'b-key', 'high-c-key', 'c-sharp-key', 'd-sharp-key', 'f-sharp-key', 'g-sharp-key', 'a-sharp-key']
+// Keys array
+const keys = ['c-key', 'd-key', 'e-key', 'f-key', 'g-key', 'a-key', 'b-key', 'high-c-key', 'c-sharp-key', 'd-sharp-key', 'f-sharp-key', 'g-sharp-key', 'a-sharp-key'];
+// Notes in a blank array
+const notes = [];
