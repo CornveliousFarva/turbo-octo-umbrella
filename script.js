29cc478de66a4afae66e00a48b6fe0e4cc2ab922
Greg Schuman <gcschuman@hotmail.com>
2023-03-09 13:04:01 -0800

    Added notes to where a couple of the initial functions will be in the script.

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 const keys = ['c-key', 'd-key', 'e-key', 'f-key', 'g-key', 'a-key', 'b-key', 'high-c-key', 'c-sharp-key', 'd-sharp-key', 'f-sharp-key', 'g-sharp-key', 'a-sharp-key'];
 // Notes in a blank array
 const notes = [];
+keys.forEach(function(key) {
+    notes.push(document.getElementById(key));
+})
+
+// Function that changes the color of the keys
+// Function with event handler properties
